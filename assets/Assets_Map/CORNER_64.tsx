18da8bdfb5062ff6e3885c6a7a1793c9d0326f89
Anonymous <anon@example.com>
2026-01-24 14:50:53 +0100

    All assests

--- assets/Assets_Map/CORNER_64.tsx
+++ assets/Assets_Map/CORNER_64.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="CORNER_64" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+<tileset version="1.10" tiledversion="1.11.2" name="CORNER_64" tilewidth="32" tileheight="32" tilecount="4" columns="2">
  <image source="CORNER_64.png" width="64" height="64"/>
 </tileset>
