f09dd26667057ab2fc428b6697c49439a3922d24
go97qev <go97qev@tum.de>
2026-01-24 16:25:55 +0100

    changed the health bar to icons, instead of text.

--- core/src/de/tum/cit/fop/maze/HUD.java
+++ core/src/de/tum/cit/fop/maze/HUD.java
@@ -1,7 +1,10 @@
 package de.tum.cit.fop.maze;
 
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -15,9 +18,12 @@ public class HUD {
     //private MazeRunnerGame game;
     private Stage stage;
     private Viewport viewport;
+    private TextureRegion heart_texture;
 
     private Label scoreLabel;
     private Label healthLabel;
+    private Table healthTable;
+
     private Label keyLabel;
     private Label levelLabel;
     private TextButton shopButton;
@@ -28,11 +34,15 @@ public class HUD {
         this.configManager = game.getConfigManager();
         viewport = new StretchViewport(1920, 1080);
         stage = new Stage(viewport);
+        TextureAtlas textureAtlas = new TextureAtlas("assets/craft/craftacular-ui.atlas");
+        heart_texture = textureAtlas.findRegion("heart");
 
         scoreLabel = new Label("Score: ", game.getSkin());
         scoreLabel.setFontScale(2.0f);
+
         healthLabel = new Label("Health: ", game.getSkin());
         healthLabel.setFontScale(2.0f);
+
         keyLabel = new Label("Key: no", game.getSkin());
         keyLabel.setFontScale(2.0f);
         levelLabel = new Label("Level: 1", game.getSkin());
@@ -41,8 +51,11 @@ public class HUD {
         Table topTable = new Table();
         topTable.setFillParent(true);
         topTable.top();
+        healthTable = new Table();
+        healthTable.add(healthLabel).left();
+
         topTable.add(levelLabel).left().expandX().pad(10);
-        topTable.add(healthLabel).left().expandX().pad(10);
+        topTable.add(healthTable).left().expandX().pad(10);
         topTable.add(scoreLabel).center().expandX().pad(10);
         topTable.add(keyLabel).right().expandX().pad(10);
 
@@ -67,9 +80,16 @@ public class HUD {
     }
 
     public void update(int levelNumber, int hp, int score, boolean hasKey){
+        healthTable.clear();
         levelLabel.setText("Level: " + levelNumber);
         scoreLabel.setText("Score: " + score);
-        healthLabel.setText("Health: " + hp);
+        healthTable.center();
+        healthTable.add(healthLabel);
+        for (int i = 0; i < hp; i++) {
+            Image heart_image = new Image(heart_texture);
+            heart_image.setScale(3.0f);
+            healthTable.add(heart_image).padLeft(50).padTop(40);
+        }
         shopButton.setText("Open Shop (" + configManager.getKeyBindingName("openShop") + ")");
         if(hasKey){
             keyLabel.setText("Has key");
